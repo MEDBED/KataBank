fc92c327c9973cbe21566ad78e66bbf823ee8378
mohamedbouhamed <med.bouhamed@live.fr>
2021-02-17 11:30:27 +0100

    data model

--- src/main/java/com/sociteGeneral/model/BankAcount.java
+++ src/main/java/com/sociteGeneral/model/BankAcount.java
@@ -1,4 +1,5 @@
 package com.sociteGeneral.model;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -24,36 +25,51 @@ public class BankAcount {
     @Column
     private float balance;
 
-    public int getAcountiId()
-    {
+    public int getAcountiId() {
         return acountId;
     }
-    public void setAcountiId(int acountId)
-    {
+
+    public void setAcountiId(int acountId) {
         this.acountId = acountId;
     }
-    public String getClientName()
-    {
+
+    public String getClientName() {
         return clientName;
     }
-    public void setClientName(String clientName)
-    {
+
+    public void setClientName(String clientName) {
         this.clientName = clientName;
     }
-    public float getAmount()
-    {
+
+    public float getAmount() {
         return amount;
     }
-    public void setAmount(float amount)
-    {
+
+    public void setAmount(float amount) {
         this.amount = amount;
     }
-    public float getBalance()
-    {
+
+    public float getBalance() {
         return balance;
     }
-    public void setBalance(float balance)
-    {
+
+    public void setBalance(float balance) {
         this.balance = balance;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
     }
 }
