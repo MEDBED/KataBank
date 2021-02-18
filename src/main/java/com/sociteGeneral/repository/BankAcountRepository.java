23c1a07f07c3c09096886d416ab67a4619c4b559
mohamedbouhamed <med.bouhamed@live.fr>
2021-02-18 16:09:24 +0100

    repository

--- src/main/java/com/sociteGeneral/repository/BankAcountRepository.java
+++ src/main/java/com/sociteGeneral/repository/BankAcountRepository.java
@@ -2,7 +2,9 @@ package com.sociteGeneral.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import com.sociteGeneral.model.BankAcount;
+import org.springframework.stereotype.Component;
 
 //repository that extends CrudRepository
+@Component
 public interface BankAcountRepository extends CrudRepository<BankAcount, Integer>  {
 }
--- src/main/java/com/sociteGeneral/repository/OperationsRepository.java
+++ src/main/java/com/sociteGeneral/repository/OperationsRepository.java
@@ -2,6 +2,8 @@ package com.sociteGeneral.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import com.sociteGeneral.model.Operations;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface OperationsRepository extends CrudRepository<Operations, Integer> {
 }
