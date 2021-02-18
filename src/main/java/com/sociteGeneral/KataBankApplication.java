876c42f8dff0d583e8feb3fdcb167bf3a4b96194
mohamedbouhamed <med.bouhamed@live.fr>
2021-02-18 15:51:27 +0100

    main config

--- src/main/java/com/sociteGeneral/KataBankApplication.java
+++ src/main/java/com/sociteGeneral/KataBankApplication.java
@@ -2,8 +2,20 @@ package com.sociteGeneral;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan("com.sociteGeneral.repository")
+@EntityScan("com.sociteGeneral.repository")
+@EnableJpaRepositories("com.sociteGeneral.repository")
+
 public class KataBankApplication {
 
 	public static void main(String[] args) {
