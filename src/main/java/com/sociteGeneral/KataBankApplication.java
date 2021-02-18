d4b351021402a3cdf9fb3bffd48a0ec6d7d16cb0
mohamedbouhamed <med.bouhamed@live.fr>
2021-02-18 15:33:24 +0100

    structure

--- src/main/java/com/sociteGeneral/KataBankApplication.java
+++ src/main/java/com/sociteGeneral/KataBankApplication.java
@@ -1,4 +1,4 @@
-package com.sociteGeneral.Katabank;
+package com.sociteGeneral;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/sociteGeneral/service/OperationsServices.java
+++ src/main/java/com/sociteGeneral/service/OperationsServices.java
@@ -3,15 +3,28 @@ package com.sociteGeneral.service;
 import com.sociteGeneral.model.BankAcount;
 import com.sociteGeneral.model.Operations;
 
+import java.util.ArrayList;
 import java.util.List;
+import com.sociteGeneral.repository.OperationsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//defining the business logic
+@Service
 
 public class OperationsServices {
-    public static BankAcount getOperationsByClientId(int clientId) {
+    @Autowired
+    OperationsRepository operationsRepository;
+
+    public static Operations getOperationsByClientId(int clientId) {
         return null;
     }
 
-    public List<BankAcount> getAllOperations() {
-        return null;
+    public List<Operations> getAllOperations() {
+
+        List<Operations> Operation = new ArrayList<Operations>();
+        operationsRepository.findAll().forEach(operations1 -> Operation.add(operations1));
+        return Operation;
     }
 
     public void delete(int clientId) {
